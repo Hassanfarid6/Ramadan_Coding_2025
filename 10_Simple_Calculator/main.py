c9bd4067e919d9e051c4866f5051409ffdeb8d60
Ali_jan <hassanfarid667@gmail.com>
2025-03-21 21:52:08 +0500

    simple Calculator updated

--- 10_Simple_Calculator/main.py
+++ 10_Simple_Calculator/main.py
@@ -1,5 +1,46 @@
 import streamlit as st
 
-st.title("Simple Calculator")
 
-num1 =
+def main():
+
+    st.title("Simple Calculator")
+    st.write("Enter two numbers and choose an operation")
+
+    col1, col2 = st.columns(2)
+
+    with col1:
+        num1 = st.number_input("Enter First Number")
+
+    with col2:
+        num2 = st.number_input("Enter Second Number")
+
+    operation = st.selectbox(
+        "Select Operation", ["Addition (+)", "Subtraction (-)", "Multiplication (*)", "Division (/)"]
+                            )
+
+    if st.button("Calculate"):
+        try:
+            if operation == "Addition (+)":
+                result = num1 + num2
+                symbol = "+"
+            elif operation == "Subtraction (-)":
+                result = num1 - num2
+                symbol = "-"
+            elif operation == "Multiplication (*)":
+                result = num1 * num2
+                symbol = "x"
+            else:
+                if num2 == 0:
+                    st.error("Cannot divide by zero")
+                    return
+                
+                result = num1 / num2
+                symbol = "/"
+
+            st.success(f"The result of {num1} {symbol} {num2} is {result}")
+
+        except Exception as e:
+            st.error(f"An error occurred: {e}")
+
+if __name__ == "__main__":
+    main()
